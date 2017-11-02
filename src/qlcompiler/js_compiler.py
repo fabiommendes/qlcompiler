52e95adc34dedb03def42a8661678078d67ab49c
João Pedro Sconetto <sconetto.joao@gmail.com>
2017-11-02 15:33:31 -0200

    Adding the basic JS lexer

--- src/qlcompiler/js_compiler.py
+++ src/qlcompiler/js_compiler.py
@@ -1,10 +1,21 @@
 from .compiler import Compiler
-
+import ox
 
 class JsCompiler(Compiler):
     """
     Javascript compiler.
     """
+
+    tokens_list = ['NUMBERS', 'STRINGS', 'BOOLEAN', 'OPERATORS']
+
+    def js_lexer:
+        lexer = ox.make_lexer([
+            ('NUMBERS', r'[+-]?\d+(?:\.\d+)'),
+            ('STRINGS', r'^([\'|\"]{1})+(?:(\w+|\W+|\d+|\D+|\s+|\S+|))+([\'|\"]{1})$'),
+            ('BOOLEAN', r'(\Atrue)|(\Afalse)'),
+            ('OPERATORS', r'[+\-*/%\|\&]'),
+        ])
+        return lexer
 
 
 def compile(ql, **kwargs):
